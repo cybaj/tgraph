64583f3d084b1eccf626481b8037791bfecaca70
Minseok Kim <minseokkim@192.168.nate.com>
2021-10-31 13:45:47 +0900

    fix index to add edge

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import TGraph from "./src/graph"
 import TNode from "./src/node"
+import TEdge from "./src/edge"
 
 export const Graph = TGraph;
 export const Node = TNode;
+export const Edge = TEdge;
 
 export default Graph
