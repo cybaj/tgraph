2b946917591cbb87fd02d1d8aa517f02874a5c01
Minseok Kim <minseokkim@192.168.nate.com>
2021-12-08 18:27:52 +0900

    add typing

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+import TGraph from "./graph";
+import TNode from "./node";
+import TEdge from "./edge";
+import { getEntries as gE, getTerminals as gT } from "./utils";
+export declare const Graph: typeof TGraph;
+export declare const Node: typeof TNode;
+export declare const Edge: typeof TEdge;
+export declare const getEntries: typeof gE;
+export declare const getTerminals: typeof gT;
+export type TNodeType<ContentType, EdgeType={}> = TNode<ContentType, EdgeType>
+export type TEdgeType<ContentType, EdgeType={}> = TEdge<ContentType, EdgeType>
+export default Graph;
