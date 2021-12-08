6d015d4d1b1aaf3999a525a79e0f7807160576e0
Minseok Kim <minseokkim@192.168.nate.com>
2021-12-08 19:35:20 +0900

    add graph type

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,6 +7,7 @@ export declare const Node: typeof TNode;
 export declare const Edge: typeof TEdge;
 export declare const getEntries: typeof gE;
 export declare const getTerminals: typeof gT;
+export type TGraphType<ContentType, EdgeType={}> = TGraph<ContentType, EdgeType>
 export type TNodeType<ContentType, EdgeType={}> = TNode<ContentType, EdgeType>
 export type TEdgeType<ContentType, EdgeType={}> = TEdge<ContentType, EdgeType>
 export default Graph;
