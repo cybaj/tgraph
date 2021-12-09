d9cdb2af4f9232f8301a3a9cc508b49a88beff96
Minseok Kim <minseokkim@192.168.nate.com>
2021-12-09 18:40:16 +0900

    add tests

--- __test__/edge.spec.ts
+++ __test__/edge.spec.ts
@@ -1,3 +1,16 @@
-describe('edge', () => {
-    it('blank', () => undefined);
+import TNode from '../src/node';
+import TEdge, {FROM} from '../src/edge';
+import type {TEdgeType} from "../src/index.d";
+
+describe('Edge test suit', () => {
+    const appleNode = new TNode<number, TEdgeType<number, TEdgeType<number>>>('apple', 0);
+    const bananaNode = new TNode<number, TEdgeType<number, TEdgeType<number>>>('banana', 1);
+    const edge = new TEdge<number, TEdgeType<number, TEdgeType<number>>, {id: string} >(bananaNode, appleNode, {id: 'down'})
+    it('single edge', () => {
+      expect(edge.name).toBe("banana"+FROM+"apple")
+      expect(edge.ancestry).toEqual(appleNode)
+      expect(edge.descendant).toEqual(bananaNode)
+      expect(edge.content).toEqual({id: 'down'})
+    }
+    );
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+# STATUS
+- 현재 node.edges 가 쓸모 없게 존재함.
+
 # TODO
-- Jest 설정하기
-- graph 생성 unit test
+[-]Jest 설정하기
+[-]graph 생성 unit test
+[]adjancency matrix 추가하기
+[]node.edges 쓸모있게 하기
